61ad843d1f3dc333feceb8c4e3e14ece59251c58
wuhe <1426912188@qq.com>
2021-03-12 01:35:14 +0800

    inti

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-tabs sticky scrollspy swipeable>
+      <van-tab title="标签 1"
+        ><div style="height: 100px; width: 100%; background-color: red;">
+          内容 1
+        </div></van-tab
+      >
+      <van-tab title="标签 2"
+        ><div style="height: 100px; width: 100%; background-color: blue;">
+          内容 2
+        </div></van-tab
+      >
+      <van-tab title="标签 3"
+        ><div style="height: 100px; width: 100%; background-color: yellow">
+          内容 3
+        </div></van-tab
+      >
+      <van-tab title="标签 4"
+        ><div style="width: 100%; background-color: green">
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell v-for="item in list" :key="item" :title="item" />
+          </van-list></div
+      ></van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    };
+  },
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 3; i++) {
+          this.list.push(this.list.length + 1);
+        }
+
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 9) {
+          this.finished = true;
+        }
+      }, 1000);
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { Tab, Tabs, List, Cell } from "vant";
 
-Vue.config.productionTip = false
+Vue.use(Tab);
+Vue.use(Tabs);
+Vue.use(List);
+Vue.use(Cell);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
